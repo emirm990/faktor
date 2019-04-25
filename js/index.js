dca102b88f934fa23c8fd78a607a57e37686eeaf
Emir Mekić <emir.mekic@live.com>
2019-04-25 16:57:50 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,57 @@
+let najcitanije = document.getElementById("najcitanije");
+let najnovije = document.getElementById("najnovije");
+let najcitanijeContent = document.getElementById("najcitanije-content");
+let najnovijeContent = document.getElementById("najnovije-content");
+let dateHolder = document.getElementById("date");
+
+let date = new Date();
+let day = "";
+getDay();
+function getDay() {
+  switch (date.getDay()) {
+    case 0:
+      day = "Nedjelja";
+      break;
+    case 1:
+      day = "Ponedjeljak";
+      break;
+    case 2:
+      day = "Utorak";
+      break;
+    case 3:
+      day = "Srijeda";
+      break;
+    case 4:
+      day = "Četvrtak";
+      break;
+    case 5:
+      day = "Petak";
+      break;
+    case 6:
+      day = "Subota";
+      break;
+  }
+}
+let formatedDate = {
+  date: date.getDate(),
+  month: date.getMonth() + 1,
+  year: date.getFullYear(),
+  day: day
+};
+dateHolder.innerHTML = `${formatedDate.day},  ${formatedDate.date}.${
+  formatedDate.month
+}.${formatedDate.year}. `;
+console.log(formatedDate);
+najcitanije.onclick = () => {
+  najcitanije.classList.add("active");
+  najnovije.classList.remove("active");
+  najcitanijeContent.classList.remove("closed");
+  najnovijeContent.classList.add("closed");
+};
+
+najnovije.onclick = () => {
+  najnovije.classList.add("active");
+  najcitanije.classList.remove("active");
+  najnovijeContent.classList.remove("closed");
+  najcitanijeContent.classList.add("closed");
+};
